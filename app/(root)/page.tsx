198dc15333221ad96ddb20c2e0d45bcedf2b78b2
Sandeep <sandy6862g22@gmail.com>
2024-05-27 22:34:26 +0530

    created the header components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import React from 'react'
+
+const Home = () => {
+  const loggedInUser = {
+    firstName: "Sandeep"
+  }
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type={"greeting"} title="Welcome" user={loggedInUser.firstName || "Guest"} subtext={"Access or manage your account and transactions efficently."} />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}  />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
