4a00e41f50cc7797d7828682ad35b2d007ad73bd
Sandeep <sandy6862g22@gmail.com>
2024-05-28 23:29:13 +0530

    created left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
+    const loggedIn = {firstName: "Sandeep", lastName: "Kumar"}
   return (
-    <main>
-        SIDEBAR
-        {children}
-    </main>
-  );
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn} />
+            {children}
+        </main>
+    );
 }
