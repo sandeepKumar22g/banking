9e3e34d28d6dee19f0df3107610d49d733372f91
Sandeep <sandy6862g22@gmail.com>
2024-05-30 15:00:15 +0530

    created right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
   const loggedInUser = {
-    firstName: "Sandeep"
+    firstName: "Sandeep",
+    lastName: "Kumar",
+    email: "contact@email.com"
   }
 
   return (
@@ -14,7 +17,9 @@ const Home = () => {
           <HeaderBox type={"greeting"} title="Welcome" user={loggedInUser.firstName || "Guest"} subtext={"Access or manage your account and transactions efficently."} />
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}  />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedInUser} transactions={[]} banks={[{}, {}]} />
     </section>
   )
 }
