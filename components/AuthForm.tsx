f42333a72e13a723ac9b317128052de07fed9f52
Sandeep <sandy6862g22@gmail.com>
2024-06-01 23:27:41 +0530

    created form label and input

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,7 +6,10 @@ import React, { useState } from 'react'
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
- 
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
 const formSchema = z.object({
     username: z.string().min(2, {
         message: "Username must be at least 2 characters.",
@@ -15,6 +18,23 @@ const formSchema = z.object({
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+
+
     return (
         <section className="auth-form">
             <header className='flex flex-col gap-5 md:gap-8'>
@@ -31,13 +51,33 @@ const AuthForm = ({ type }: { type: string }) => {
                     </h1>
                 </div>
             </header>
-            { user ? (
+            {user ? (
                 <div className='flex flex-col gap-4'>
                     {/* plade link */}
                 </div>
-            ): (
+            ) : (
                 <>
-
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="shadcn" {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is your public display name.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
                 </>
             )}
 
