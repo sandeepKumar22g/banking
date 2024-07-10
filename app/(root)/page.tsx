dba4c003425c31965a10cb2bdf9c477eb5f74001
Sandeep <sandy6862g22@gmail.com>
2024-07-10 13:12:34 +0530

    get the logged in user details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,17 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/users.actions'
 import React from 'react'
 
-const Home = () => {
-  const loggedInUser = {
-    firstName: "Sandeep",
-    lastName: "Kumar",
-    email: "contact@email.com"
-  }
+const Home = async () => {
+  const loggedInUser = await getLoggedInUser();
 
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          <HeaderBox type={"greeting"} title="Welcome" user={loggedInUser.firstName || "Guest"} subtext={"Access or manage your account and transactions efficently."} />
+          <HeaderBox type={"greeting"} title="Welcome" user={loggedInUser?.name || "Guest"} subtext={"Access or manage your account and transactions efficently."} />
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35}  />
         </header>
         RECENT TRANSACTIONS
