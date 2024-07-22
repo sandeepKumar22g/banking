d9bfc9daa23d75294bd592e8692d3732ff9335f3
Sandeep <sandy6862g22@gmail.com>
2024-07-22 12:16:34 +0530

    created the payment transfer form and logic

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,9 +1,25 @@
+import HeaderBox from '@/components/HeaderBox'
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/users.actions';
 import React from 'react'
 
-const PaymentTransfer = () => {
-  return (
-    <div>PaymentTransfer</div>
-  )
+const PaymentTransfer = async() => {
+    const loggedInUser = await getLoggedInUser();
+    const accounts = await getAccounts({userId: loggedInUser?.$id!})
+
+    if(!accounts) return;
+
+    const accountsData = accounts?.data
+
+    return (
+        <section className='payment-transfer'>
+            <HeaderBox title='Payment Transfer' subtext='Please provide any specific details or notes related to the payment transfer' />
+            <section className='size-full pt-5'>
+                <PaymentTransferForm accounts={accountsData} />
+            </section>
+        </section>
+    )
 }
 
 export default PaymentTransfer
